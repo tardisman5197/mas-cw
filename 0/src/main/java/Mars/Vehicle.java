21a6e3615bd72503480f43e06b1bc7eff33ec74f
Edward Evans <tardisman5197@gmail.com>
2018-11-08 14:42:19 +0000

    Init commit

---
+++ 0/src/main/java/Mars/Vehicle.java
@@ -0,0 +1,27 @@
+package Mars;
+
+import java.util.ArrayList;
+
+class Vehicle extends Entity{
+	public boolean carryingSample;
+	
+	public Vehicle(Location l){
+		super(l);	
+		this.carryingSample = false;
+	}
+
+	public void act(Field f, Mothership m, ArrayList<Rock> rocksCollected)
+	{
+		actCollaborative(f,m,rocksCollected);
+		//actSimple(f,m,rocksCollected);
+	}
+	
+	
+	public void actCollaborative(Field f, Mothership m, ArrayList<Rock> rocksCollected){
+		//complete this method
+	}
+
+	public void actSimple(Field f, Mothership m, ArrayList<Rock> rocksCollected){
+		//complete this method
+	}
+}
